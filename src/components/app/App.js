abbdcb8ce2eeff0a1ddd89f36b713573cb53250e
Anton <reopik@mail.ru>
2023-07-01 23:55:23 +0300

    created some structure elements and part of section 1

---
+++ src/components/app/App.js
@@ -0,0 +1,11 @@
+import Section1 from "../section1/Section1";
+
+const App = () => {
+    return (
+        <div className="app">
+            <Section1 />
+        </div>
+    );
+}
+
+export default App;
